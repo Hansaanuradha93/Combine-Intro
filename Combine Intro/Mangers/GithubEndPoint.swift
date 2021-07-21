e046a1e974e8b223389083ab76fd4998086f8c38
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-07-21 21:15:55 +0530

    Github Endpoint and get followers method added

---
+++ Combine Intro/Mangers/GithubEndPoint.swift
@@ -0,0 +1,76 @@
+import Foundation
+
+// MARK: Cases
+enum GithubEndPoint {
+    case followers(username: String, perPage: Int, page: Int)
+    case user(username: String)
+}
+
+
+// MARK: - Scheme
+extension GithubEndPoint {
+    private var scheme: String { return "https" }
+}
+
+
+// MARK: - Host
+extension GithubEndPoint {
+    private var host: String { return "api.github.com" }
+}
+
+
+// MARK: - Path
+extension GithubEndPoint {
+    
+    private var path: String {
+        switch self {
+        case .followers(let username, _, _): return "/users/\(username)/followers"
+        case .user(let username): return "/users/\(username)"
+        }
+    }
+}
+
+
+// MARK: - Parameters
+extension GithubEndPoint {
+    
+    private var parameters: [String : Any] {
+        switch self {
+        case .followers( _, let perPage, let page):
+            let parameters: [String : Any] = [
+                "per_page" : perPage,
+                "page" : page
+            ]
+            return parameters
+        case .user( _): return ["":""]
+        }
+    }
+}
+
+
+// MARK: - Query Components
+extension GithubEndPoint {
+    
+    private var queryComponents: [URLQueryItem] {
+        var components = [URLQueryItem]()
+        for(key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.append(queryItem)
+        }
+        return components
+    }
+}
+
+
+// MARK: - Url
+extension GithubEndPoint {
+    
+    var url: URL {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = host
+        components.path = path
+        components.queryItems = queryComponents
+        return components.url!
+    }
+}
--- Combine Intro/Mangers/NetworkManager.swift
+++ Combine Intro/Mangers/NetworkManager.swift
@@ -6,6 +6,8 @@ class NetworkManager {
     static let shared = NetworkManager()
     
     private init() {}
+    
+    private let perPage = 10
 }
 
 // MARK: - Methods
@@ -17,5 +19,17 @@ extension NetworkManager {
                 promise(.success(["Apple", "Google", "Facebook", "Microsoft"]))
             }
         }
+    }
+
+    
+    func getFollwers(for username: String, page: Int) -> AnyPublisher<[Follower], Never> {
+        let endPoint = GithubEndPoint.followers(username: username, perPage: perPage, page: page)
+        let url = endPoint.url
+
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: [Follower].self, decoder: JSONDecoder())
+            .replaceError(with: [Follower]())
+            .eraseToAnyPublisher()
     }
 }
