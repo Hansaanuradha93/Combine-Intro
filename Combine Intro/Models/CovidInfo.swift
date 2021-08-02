e43bf597a955a9d6be54acea18c94365ffaaea96
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-08-02 19:04:35 +0530

    CovidInfo model added

---
+++ Combine Intro/Models/CovidInfo.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+struct CovidInfo: Codable, Hashable {
+    let length: Int?
+    let maxPageLimit: Int?
+    let totalRecords: Int?
+    let data: [DailyRecord]
+}
+
+struct DailyRecord: Codable, Hashable {
+    let date: String?
+    let name: String?
+    let casesDaily: Int?
+    let casesCumulative: Int?
+    let deathsDaily: Int?
+    let deathsCumulative: Int?
+    let firstVaccinationsDaily: Int?
+    let firstVaccinationsCumulative: Int?
+    let secondVaccinationsDaily: Int?
+    let secondVaccinationsCumulative: Int?
+}
